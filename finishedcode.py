6095287397acafe91d846a1ad44eac12ca35a55a
starry-skies <mmazin2028@kehillahstudent.org>
2024-10-10 11:30:26 -0700

    finished code

---
+++ finishedcode.py
@@ -0,0 +1,11 @@
+import turtle
+#defining what draw_square is
+def draw_square():
+ for i in range(4):
+  turtle.forward(90)
+  turtle.right(90)
+#calling draw_square
+draw_square()
+
+
+turtle.exitonclick()
